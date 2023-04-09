38e89f0c86bec3962b1ea8c68fa52dfd6aa6c204
Jacob Mealey <jacobmealey.maine@gmail.com>
2023-04-09 19:57:43 -0400

    drawing single spherew

--- main.cu
+++ main.cu
@@ -2,80 +2,27 @@
 
 #include "ray.h"
 #include "vec3.h"
-//#include "hittable_list.h"
-//#include "sphere.h"
 //#include "const.h"
 //#include "camera.h"
 //#include "material.h"
 //#include "texture.h"
 
-/*
-color ray_color(const ray &r, const hittable &world, int depth) {
-    hit_record rec;
 
-    if(depth <= 0)
-        return color(0, 0, 0);
+__device__ bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
 
-    if(world.hit(r, 0.001, infinity, rec)){
-        ray scattered;
-        color attenuation;
-        if(rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-            return attenuation * ray_color(scattered, world, depth-1);
-        return color(0, 0, 0);
-    }
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * color(1, 1, 1) + t*color(0.5, 0.7, 1.0);
+    auto discriminant = half_b*half_b  - a*c;
+    // didn't hit spere
+    return(discriminant > 0);
+    
 }
-
-hittable_list random_scene() {
-    hittable_list world;
-
-    const double aspect_ratio = 3.0/2.0;
-    camera cam(point3(12, 2, 3), point3(0, 0, 0), vec3(0, 1, 0), 30, aspect_ratio);
-    auto ground_material = std::make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(std::make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
-
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
-            auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
-
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-                std::shared_ptr<material> sphere_material;
-
-                if (choose_mat < 0.8) {
-                    // diffuse
-                    auto albedo = color::random() * color::random();
-                    sphere_material = std::make_shared<lambertian>(albedo);
-                    world.add(std::make_shared<sphere>(center, 0.2, sphere_material));
-                } else {
-                    // metal
-                    auto albedo = color::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = std::make_shared<metal>(albedo, fuzz);
-                    world.add(std::make_shared<sphere>(center, 0.2, sphere_material));
-                }           
-            }
-        }
-    }
-
-    // auto material1 = make_shared<dielectric>(1.5);
-    // world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-    auto material2 = std::make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(std::make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-    auto material3 = std::make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(std::make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-
-    return world;
-}
-
-*/
-
 
 __device__ color ray_color(const ray &r) {
+    if(hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5f*(unit_direction.y() + 1.0f);
     return (1.0 - t)*color(1.0f, 1.0f, 1.0f) + t*color(0.5f, 0.7f, 1.0f);
@@ -88,6 +35,8 @@ __global__ void render(color *buff, int width, int height,
     if((i >= width) || (j >= height)) 
         return;
 
+
+
     int loc = j*width + i;
     float u = float(i) / width;
     float v = float(j) / height;
@@ -99,9 +48,9 @@ __global__ void render(color *buff, int width, int height,
 int main() {
 
     // Image :)
-    const double aspect_ratio = 2.0/3.0;
+    const double aspect_ratio = 16.0/9.0;
     int image_width = 600;
-    int image_height = image_width*aspect_ratio;
+    int image_height = image_width/aspect_ratio;
     int nx = image_width;
     int ny = image_height;
     int tx = 8;
@@ -120,7 +69,7 @@ int main() {
     // create pixel buffer
     color *cuda_buff;
     float viewport_height = 2.0;
-    float viewport_width = viewport_height*aspect_ratio; 
+    float viewport_width = viewport_height/aspect_ratio; 
     float focal_length = 1.0;
     
     cudaError_t result = cudaMallocManaged((void **) &cuda_buff, size*sizeof(color));
@@ -129,7 +78,8 @@ int main() {
         exit(1);
     }
     render<<<blocks, threads>>>(cuda_buff, image_width, image_height, 
-            vec3(0, 0, -2), vec3(viewport_width, 0, 0), vec3(0, viewport_height, 0), vec3(0, 0, 0));
+            vec3(-2, -1, -1), 
+            vec3(4, 0, 0), vec3(0, 2, 0), vec3(0, 0, 0));
 
     cudaDeviceSynchronize();
     //result = cudaMemcpy(buffer, cuda_buff, size * sizeof(float), cudaMemcpyDeviceToHost);
