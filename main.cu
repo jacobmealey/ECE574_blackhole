eb2d37e26320c8bb38ae2ff38027a1f50118963d
Jacob Mealey <jacobmealey.maine@gmail.com>
2023-04-10 10:41:07 -0400

    working-ish raytracing with hittable_lists

--- main.cu
+++ main.cu
@@ -2,47 +2,71 @@
 
 #include "ray.h"
 #include "vec3.h"
+#include "hittable.h"
+#include "sphere.h"
+#include "hittable_list.h"
 //#include "const.h"
 //#include "camera.h"
 //#include "material.h"
 //#include "texture.h"
 
 
-__device__ bool hit_sphere(const point3& center, double radius, const ray& r) {
+__device__ float hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
     auto a = r.direction().length_squared();
     auto half_b = dot(oc, r.direction());
     auto c = dot(oc, oc) - radius*radius;
 
-    auto discriminant = half_b*half_b  - a*c;
+    auto discriminant = (half_b)*(half_b)  - a*c;
     // didn't hit spere
-    return(discriminant > 0);
+    if(discriminant < 0){
+        return -1.0f;
+    }else {
+        return (-half_b -sqrtf(discriminant))/(a);
+    }
+
     
 }
 
-__device__ color ray_color(const ray &r) {
-    if(hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
+__device__ color ray_color(const ray &r, hittable **world) {
+    //auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    //if(t > 0){
+    //    vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+    //    return 0.5*color(N.x() + 1, N.y() + 1, N.z() + 1);
+
+    //}
+
+    hit_record rec;
+    if((*world)->hit(r, 0, infinity, rec)){
+        return 0.5*(rec.normal + color(1, 1, 1));
+    }
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5f*(unit_direction.y() + 1.0f);
     return (1.0 - t)*color(1.0f, 1.0f, 1.0f) + t*color(0.5f, 0.7f, 1.0f);
 }
 
+// list is a pointer to a list of all element
+__global__ void create_world(hittable **list, hittable **world) {
+    if(threadIdx.x == 0 && blockIdx.x == 0){
+        *list = new sphere(vec3(0, 0, -1), 0.5);
+        *(list + 1) = new sphere(vec3(0, -100.5, -1), 100);
+        *world = new hittable_list(list, 2);
+    }
+}
+
 __global__ void render(color *buff, int width, int height,  
-        vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin){
+        vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin, hittable **world){
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= width) || (j >= height)) 
         return;
-
-
 
     int loc = j*width + i;
     float u = float(i) / width;
     float v = float(j) / height;
 
     //buff[loc] = color(float(i)/width, float(j)/height, 0.2);
-    buff[loc] = ray_color(ray(origin, lower_left_corner + u*horizontal + v*vertical));
+    buff[loc] = ray_color(ray(origin, lower_left_corner + u*horizontal + v*vertical), world);
 }
 
 int main() {
@@ -69,8 +93,13 @@ int main() {
     // create pixel buffer
     color *cuda_buff;
     float viewport_height = 2.0;
-    float viewport_width = viewport_height/aspect_ratio; 
-    float focal_length = 1.0;
+    float viewport_width = viewport_height*aspect_ratio; 
+    hittable **hit_list;
+    cudaMalloc((void**) &hit_list, 2*sizeof(hittable *));
+    hittable **world;
+    cudaMalloc((void**) &world, sizeof(hittable *));
+
+    create_world<<<blocks, threads>>>(hit_list, world);
     
     cudaError_t result = cudaMallocManaged((void **) &cuda_buff, size*sizeof(color));
     if(result) {
@@ -78,8 +107,8 @@ int main() {
         exit(1);
     }
     render<<<blocks, threads>>>(cuda_buff, image_width, image_height, 
-            vec3(-2, -1, -1), 
-            vec3(4, 0, 0), vec3(0, 2, 0), vec3(0, 0, 0));
+            vec3(-viewport_width/2, -viewport_height/2, -1), 
+            vec3(4, 0, 0), vec3(0, 2, 0), vec3(0, 0, 0), world);
 
     cudaDeviceSynchronize();
     //result = cudaMemcpy(buffer, cuda_buff, size * sizeof(float), cudaMemcpyDeviceToHost);
