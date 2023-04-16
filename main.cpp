4e21f74773f9a7bc90fcb1ec45965bf3188f38ba
Jacob Mealey <jacobmealey.maine@gmail.com>
2023-04-16 11:27:21 -0400

    added dielectrics and sucH

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ hittable_list random_scene() {
     hittable_list world;
 
     const double aspect_ratio = 3.0/2.0;
-    camera cam(point3(12, 2, 3), point3(0, 0, 0), vec3(0, 1, 0), 30, aspect_ratio);
+    //camera cam(point3(12, 2, 3), point3(0, 0, 0), vec3(0, 1, 0), 30, aspect_ratio);
     auto ground_material = std::make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.add(std::make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
@@ -50,19 +50,22 @@ hittable_list random_scene() {
                     auto albedo = color::random() * color::random();
                     sphere_material = std::make_shared<lambertian>(albedo);
                     world.add(std::make_shared<sphere>(center, 0.2, sphere_material));
-                } else {
-                    // metal
+                } else if(choose_mat < 0.95) {// metal
                     auto albedo = color::random(0.5, 1);
                     auto fuzz = random_double(0, 0.5);
                     sphere_material = std::make_shared<metal>(albedo, fuzz);
                     world.add(std::make_shared<sphere>(center, 0.2, sphere_material));
-                }           
+                } else {
+                    // glass
+                    sphere_material = std::make_shared<dielectric>(1.5);
+                    world.add(std::make_shared<sphere>(center, 0.2, sphere_material));
+                } 
             }
         }
     }
 
-    // auto material1 = make_shared<dielectric>(1.5);
-    // world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+    auto material1 = std::make_shared<dielectric>(1.5);
+    world.add(std::make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
     auto material2 = std::make_shared<lambertian>(color(0.4, 0.2, 0.1));
     world.add(std::make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
@@ -76,13 +79,13 @@ hittable_list random_scene() {
 int main() {
 
     // Image :)
-    const double aspect_ratio = 3.0/2.0;
-    const int image_width = 400;
+    const double aspect_ratio = 16.0/9.0;
+    const int image_width = 800;
     const int image_height = static_cast<int>(image_width/aspect_ratio);
-    const int spp = 50;
-    const int max_depth = 150;
+    const int spp = 100;
+    const int max_depth = 50;
 
-    camera cam(point3(12, 2, 3), point3(0, 0, 0), vec3(0, 1, 0), 30, aspect_ratio);
+    camera cam(point3(12, 2, 3), point3(0, 0, 0), vec3(0, 1, 0), 30, aspect_ratio, 0.1, 10);
     // Worldly
     hittable_list world = random_scene();
     
@@ -90,28 +93,20 @@ int main() {
     color *buffer = (color *)malloc(image_height*image_width*sizeof(color));
 
     // Generate the images in a buffer
-#pragma omp parallel  
-    {
-
-#pragma omp for
     for(int j = image_height- 1; j >= 0; j--) {
-        //std::cerr << "\rScanlines remaning: " << j << ' ' << std::flush;
+        std::cerr << "\rScanlines remaning: " << j << ' ' << std::flush;
         for(int i = 0; i < image_width; ++i) {
             color pixel_color(0, 0, 0);
-            //for(int s = 0; s < spp; s++){
-            //    auto u = (i + random_double()) / (image_width - 1);
-            //    auto v = (j + random_double()) / (image_height - 1);
-            //    ray r = cam.get_ray(u, v);
-            //    pixel_color = pixel_color +  ray_color(r, world, max_depth);
-            //}
-            auto u = (i * 1.0) / (image_width - 1);
-            auto v = (j * 1.0) / (image_height - 1);
-            ray r = cam.get_ray(u, v);
-            pixel_color = ray_color(r, world, max_depth) * 10;
+            for(int s = 0; s < spp; s++){
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color = pixel_color +  ray_color(r, world, max_depth);
+            }
             buffer[j * image_width + i] = pixel_color;
         }
     }
-    }
+
 
     // Write buffer to ppm format and stdout
     std::cout << "P3\n" << image_width << ' ' << image_height<< "\n255" << std::endl;
